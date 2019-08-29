7d6541286d7dada42531086fe6fa1905d6e28f03
saraervik <sara.ervik@r2m.se>
2019-08-29 13:52:07 +0200

    Add Alert component

---
+++ src/Alert.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import classNames from 'classnames'
+
+const Alert = (props) => {
+
+  const classes = classNames(
+    'alert',
+    props.color === 'success' && 'alert-success',
+    props.color === 'danger' && 'alert-danger',
+    (props.color === 'info' || !props.color) && 'alert-info' // default value is info
+  )
+
+  return <div aria-live='polite' role='alert' class={classes}>
+    {props.children}
+  </div>
+}
+
+export default Alert
