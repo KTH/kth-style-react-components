e52ca48c1db75574657ab3d28c6914195f7eb64c
Stefan Sundkvist <sst@kth.se>
2019-12-08 13:41:54 +0100

    Cease fetching Cortina blocks in endless loop

--- src/lib/Footer.js
+++ src/lib/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 import cortina from 'kth-node-cortina-block'
 
 class Footer extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = { footer: '' }
   }
 
@@ -11,10 +11,6 @@ class Footer extends React.Component {
     cortina({ url: this.props.url, language: this.props.language }).then(blocks =>
       this.setState({ footer: blocks.footer })
     )
-  }
-
-  componentDidUpdate() {
-    this.componentDidMount()
   }
 
   render() {
