f37589c7af3d3d29d565171e9e9f148d2f5e6bcf
Elena Rakhimova <elenara@kth.se>
2020-01-07 17:18:58 +0100

    removed FileInput

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,9 +2,8 @@ import Alert from './Alert'
 import Collapse from './Collapse'
 import InfoModalButton from './InfoModalButton'
 import ActionModalButton from './ActionModalButton'
-import FileInput from './FileInput'
 import Footer from './Footer'
 import Header from './Header'
 import PageTitle from './PageTitle'
 
-export { Alert, InfoModalButton, ActionModalButton, Collapse, FileInput, Footer, Header, PageTitle }
+export { Alert, InfoModalButton, ActionModalButton, Collapse, Footer, Header, PageTitle }
