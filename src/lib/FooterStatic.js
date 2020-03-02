0404f8872b1cdb9d730fda5a6a8f6f8a31d1d0e7
Bahadur Singh <46959789+bahadursinghSE@users.noreply.github.com>
2020-03-02 15:56:24 +0100

    Msx 40 (#10)
    * Added header data in messages.js
    
    * version set to 0.8.0
    
    * Corrected the path for i18n import.
    
    * Build and deleted FooterStaticEn.js file

--- src/lib/FooterStatic.js
+++ src/lib/FooterStatic.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
-import i18n from '../../i18n'
+import i18n from '../i18n'
 
 class FooterStatic extends React.Component {
   constructor(props) {
