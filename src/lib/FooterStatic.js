f457691b9721156d7816467a24900a10cb3154ae
Stefan Sundkvist <sst@kth.se>
2020-09-09 21:55:56 +0200

    Update to latest kth-node-cortina-block and kth-node-log Remove unused dependency on kth-style

--- src/lib/FooterStatic.js
+++ src/lib/FooterStatic.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import classNames from 'classnames'
 import i18n from './i18n'
 
 class FooterStatic extends React.Component {
--- src/lib/HeaderStatic.js
+++ src/lib/HeaderStatic.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import cortina from 'kth-node-cortina-block'
 import i18n from './i18n'
 
 class HeaderStatic extends React.Component {
