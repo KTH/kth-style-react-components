d1d9c35bb7a78ee324f005baa2edb8a4fd951911
Jonolov <jon.stjarnstrom@gmail.com>
2021-02-24 23:48:32 +0100

    Input now with reactstrap

--- src/components/formik/Input/Input.jsx
+++ src/components/formik/Input/Input.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useField, useFormikContext, getIn } from 'formik'
-import { Form, InputGroup } from 'react-bootstrap'
+import { Label, Input, FormFeedback } from 'reactstrap'
 import { Badges, ClickableDiv } from '../../helpers'
 
 export default ({ label, badges, onInfoClick, helpText, ...props }) => {
@@ -21,19 +21,19 @@ export default ({ label, badges, onInfoClick, helpText, ...props }) => {
 
   return (
     <>
-      <Form.Label htmlFor={props.name}>
+      <Label htmlFor={props.name}>
         {label}
         <Badges content={badges} />
         {typeof onInfoClick === 'function' && (
           <ClickableDiv className="glyphicon glyphicon-info-sign" onClick={onInfoClick} />
         )}
-        <Form.Control isInvalid={hasError} {...internalField} {...props} />
+        <Input invalid={hasError} {...internalField} {...props} />
         {hasError || statusInternal ? (
-          <Form.Control.Feedback style={{ display: 'block' }} type="invalid" className="error">
+          <FormFeedback style={{ display: 'block' }} type="invalid" className="error">
             {internalMeta.error || getIn(statusInternal, internalField.name)}
-          </Form.Control.Feedback>
+          </FormFeedback>
         ) : null}
-      </Form.Label>
+      </Label>
     </>
   )
 }
